9388d0c2eaf4f7732465690ef2865314a4521439
zafkielkurumi <563693600@qq.com>
2020-07-22 16:22:55 +0800

    add

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,11 +1,10 @@
-import {Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { Result } from '../models';
 
-
 export const testRouter = Router({
-    strict: true,
+  strict: true,
 });
 
 testRouter.get('/', (req: Request, res: Response) => {
-    res.send(Result.success<string>('测试成功'));
+  res.send(Result.success<string>('测试成功'));
 });
